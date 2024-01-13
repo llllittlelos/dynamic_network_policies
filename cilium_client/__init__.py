842241302e46142bb4d1aef8d8571543c558c9ad
Deathstroke <776022703@qq.com>
2024-01-13 21:36:40 +0800

    添加cilium_client模块的初始代码

---
+++ cilium_client/__init__.py
@@ -0,0 +1,3 @@
+# cilium_client/__init__.py
+
+from .cilium_client import CiliumClient
---
+++ cilium_client/cilium_client.py
@@ -0,0 +1,39 @@
+# cilium_client/cilium_client.py
+
+import pathlib
+import requests
+from configparser import ConfigParser
+
+
+class CiliumClient:
+    def __init__(self, config_file=pathlib.Path(__file__).parent.absolute() / "../config.ini"):
+        self.config = self._read_config(config_file)
+
+    @staticmethod
+    def _read_config(config_file):
+        config = ConfigParser()
+        config.read(config_file)
+
+        cluster_section = config["cluster"]
+        hosts = [host.strip() for host in cluster_section.get("hosts", "").split(",")]
+        port = int(cluster_section.get("port", "64444"))
+
+        return {
+            "hosts": hosts,
+            "port": port
+        }
+
+    def get_hosts(self) -> list:
+        return self.config["hosts"]
+
+    def get_endpoints(self, host: str = None) -> dict:
+        host = host or self.config["hosts"][0]
+        endpoint_url = f"http://{host}:{self.config['port']}/endpoints"
+        response = requests.get(endpoint_url)
+        return response.json()
+
+    def get_services(self, host: str = None) -> dict:
+        host = host or self.config["hosts"][0]
+        services_url = f"http://{host}:{self.config['port']}/services"
+        response = requests.get(services_url)
+        return response.json()
