e6a6b2433ce9679b58d0ac2ee48dcf5e84c69a5e
Deathstroke <776022703@qq.com>
2024-01-21 20:11:52 +0800

    完成基本功能

--- __main__.py
+++ __main__.py
@@ -1,16 +1,53 @@
-# 这是一个示例 Python 脚本。
+import pathlib
+from cilium_client import CiliumClient
+from microservices_yaml_handler import MicroservicesYamlHandler
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+microservices = ("bookinfo", "online boutique", "sock shop")
 
 
-# 按装订区域中的绿色按钮以运行脚本。
+def check_create_directory(_microservice_name):
+    current_directory = pathlib.Path(__file__).parent.resolve()
+    output_directory = current_directory / "output"
+    microservice_output_directory = output_directory / _microservice_name
+    cilium_client_directory = microservice_output_directory / "cilium_client"
+
+    if not output_directory.exists():
+        try:
+            output_directory.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print(f"error creating output directory: {e}")
+
+    if not microservice_output_directory.exists():
+        try:
+            microservice_output_directory.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print(f"error creating microservice output directory: {e}")
+
+    if not cilium_client_directory.exists():
+        try:
+            cilium_client_directory.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print(f"error creating cilium_client directory: {e}")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    microservice_name: str = microservices[0]
+    check_create_directory(microservice_name)
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    # cilium_client = CiliumClient()
+    # print("Hosts:", cilium_client.get_hosts())
+    #
+    # endpoints_json = cilium_client.get_endpoints_raw_json()
+    # print("Endpoints:", endpoints_json)
+    #
+    # services_json = cilium_client.get_services_raw_json()
+    # print("Services:", services_json)
+
+    microservices_yaml_handler = MicroservicesYamlHandler(microservice_name)
+
+    microservices_yaml_handler.enrich_containers()
+    microservices_yaml_handler.enrich_services()
+    microservices_yaml_handler.enrich_pod()
+
+    print(len(microservices_yaml_handler.microservices_yaml_contents))
+    print(microservices_yaml_handler.get_all_kinds())
