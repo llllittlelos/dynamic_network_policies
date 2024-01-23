08639e8bf0687c2b2d01f9a82f36b9cd2a67cf61
Deathstroke <776022703@qq.com>
2024-01-23 18:50:26 +0800

    修改cilium client的调用方式，以及保存到文件

--- cilium_inspector/cilium_inspector.go
+++ cilium_inspector/cilium_inspector.go
@@ -4,25 +4,89 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"io/ioutil"
 
 	"github.com/cilium/cilium/pkg/client"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	http.HandleFunc("/endpoints", getEndpointsHandler)
-	http.HandleFunc("/services", getServicesHandler)
+	ciliumClient, err := client.NewDefaultClient()
+	if err != nil {
+		panic(err)
+	}
+	saveEndpointsToFile(ciliumClient)
+	saveServicesToFile(ciliumClient)
+
+	http.HandleFunc("/endpoints", func(w http.ResponseWriter, r *http.Request) {
+		getEndpointsHandler(ciliumClient, w, r)
+	})
+	http.HandleFunc("/services", func(w http.ResponseWriter, r *http.Request) {
+		getServicesHandler(ciliumClient, w, r)
+	})
 
 	fmt.Println("server is running on port: 64444")
 	http.ListenAndServe(":64444", nil)
 }
 
-func getEndpointsHandler(w http.ResponseWriter, r *http.Request) {
-	ciliumClient, err := client.NewDefaultClient()
+func saveEndpointsToFile(c *client.Client) {
+	endpoints, err := c.EndpointList()
 	if err != nil {
 		panic(err)
 	}
 
-	endpoints, err := ciliumClient.EndpointList()
+	jsonData, err := json.Marshal(endpoints)
+	if err != nil {
+		panic(err)
+	}
+
+	var yamlData interface{}
+	err = yaml.Unmarshal(jsonData, &yamlData)
+	if err != nil {
+		panic(err)
+	}
+
+	yamlBytes, err := yaml.Marshal(yamlData)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("endpoints.yaml", yamlBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func saveServicesToFile(c *client.Client) {
+	services, err := c.GetServices()
+	if err != nil {
+		panic(err)
+	}
+
+	jsonData, err := json.Marshal(services)
+	if err != nil {
+		panic(err)
+	}
+
+	var yamlData interface{}
+	err = yaml.Unmarshal(jsonData, &yamlData)
+	if err != nil {
+		panic(err)
+	}
+
+	yamlBytes, err := yaml.Marshal(yamlData)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("services.yaml", yamlBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getEndpointsHandler(c *client.Client, w http.ResponseWriter, r *http.Request) {
+	endpoints, err := c.EndpointList()
 	if err != nil {
 		panic(err)
 	}
@@ -41,13 +105,8 @@ func getEndpointsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(responseJSON)
 }
 
-func getServicesHandler(w http.ResponseWriter, r *http.Request) {
-	ciliumClient, err := client.NewDefaultClient()
-	if err != nil {
-		panic(err)
-	}
-
-	services, err := ciliumClient.GetServices()
+func getServicesHandler(c *client.Client, w http.ResponseWriter, r *http.Request) {
+	services, err := c.GetServices()
 	if err != nil {
 		panic(err)
 	}
