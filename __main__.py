b389104c14ad856b037ebf3c4150583501039cb2
Deathstroke <776022703@qq.com>
2024-01-25 19:32:02 +0800

    完成风险路径生成的所有基本功能

--- __main__.py
+++ __main__.py
@@ -1,8 +1,15 @@
 import pathlib
+import shutil
+import yaml
+
 from cilium_client import CiliumClient
 from microservices_yaml_handler import MicroservicesYamlHandler
+from microservices_topology import MicroservicesTopology
+from utils import utils
 
 microservices = ("bookinfo", "online boutique", "sock shop")
+# 不同微服务的YAML文件的排除项，即不需要进行处理的YAML文件
+exclusions = {"bookinfo": ["bookinfo-psa.yaml", "bookinfo-dualstack.yaml"]}
 
 
 def check_create_directory(_microservice_name):
@@ -30,24 +37,103 @@ def check_create_directory(_microservice_name):
             print(f"error creating cilium_client directory: {e}")
 
 
+def output_for_test(test_microservices_yaml_handler, test_cilium_client, remove_test_output=False):
+    current_directory = pathlib.Path(__file__).parent.resolve()
+    output_directory = current_directory / "output"
+    test_directory = output_directory / test_microservices_yaml_handler.microservices_name / "test"
+
+    if remove_test_output:
+        try:
+            # 递归删除目录及其内容
+            shutil.rmtree(test_directory)
+            print(f"directory {test_directory} removed successfully.")
+        except OSError as e:
+            print(f"error: {e}")
+
+        return
+
+    if not test_directory.exists():
+        try:
+            test_directory.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print(f"error creating output directory: {e}")
+
+    with open(test_directory / "yaml_result.yaml", "w") as file:
+        yaml.dump(test_microservices_yaml_handler.microservices_yaml_contents, file, default_flow_style=False)
+    with open(test_directory / "services_result.yaml", "w") as file:
+        yaml.dump(test_cilium_client.get_service_items_by_namespace(microservice_namespace),
+                  file, default_flow_style=False)
+    with open(test_directory / "endpoints_result.yaml", "w") as file:
+        yaml.dump(test_cilium_client.get_endpoint_items_by_namespace(microservice_namespace),
+                  file, default_flow_style=False)
+
+
 if __name__ == '__main__':
+    force = False
+    pic_output = False
+    html_output = False
+    test_yaml_output = False
+
     microservice_name: str = microservices[0]
+    microservice_namespace = microservice_name
     check_create_directory(microservice_name)
 
-    # cilium_client = CiliumClient()
-    # print("Hosts:", cilium_client.get_hosts())
-    #
-    # endpoints_json = cilium_client.get_endpoints_raw_json()
-    # print("Endpoints:", endpoints_json)
-    #
-    # services_json = cilium_client.get_services_raw_json()
-    # print("Services:", services_json)
+    cilium_client = CiliumClient(microservice_name, force)
 
-    microservices_yaml_handler = MicroservicesYamlHandler(microservice_name)
-
+    microservices_yaml_handler = MicroservicesYamlHandler(microservice_name, exclusions[microservice_name], force)
     microservices_yaml_handler.enrich_containers()
     microservices_yaml_handler.enrich_services()
     microservices_yaml_handler.enrich_pod()
+    microservices_yaml_handler.calculate_global_score()
 
-    print(len(microservices_yaml_handler.microservices_yaml_contents))
-    print(microservices_yaml_handler.get_all_kinds())
+    raw_services_inner_ip_list = []
+    for service_item in cilium_client.get_service_items_by_namespace(microservice_namespace):
+        backend_address_set = set()
+        for backend_address in service_item["spec"]["backend-addresses"]:
+            backend_address_set.add(backend_address["ip"])
+        raw_services_inner_ip_list.append(backend_address_set)
+
+    frozen_sets = [frozenset(s) for s in raw_services_inner_ip_list]
+    unique_sets = set(frozen_sets)
+    services_inner_ip_list = [set(s) for s in unique_sets]
+
+    # 表示的包含了大于1个IP的服务，即有多个后端IP的服务
+    services_inner_ip_list = [s for s in services_inner_ip_list if len(s) > 1]
+
+    data_for_graph = {}
+    # endpoint可以视作pod的接口实现，所以直接遍历endpoint即可
+    endpoint_items = cilium_client.get_endpoint_items_by_namespace(microservice_namespace)
+    for endpoint_item in endpoint_items:
+        pod_name = endpoint_item["status"]["external-identifiers"]["k8s-pod-name"]
+        data_for_graph[pod_name] = {}
+        container_info_list = microservices_yaml_handler.get_container_info_by_pod_name(pod_name)
+        pod_container_image_name_set = set()
+        for container_info in container_info_list:
+            for item in container_info:
+                if "image" in item:
+                    pod_container_image_name_set.add(item["image"])
+        pod_container_image_name_list = list(pod_container_image_name_set)
+        data_for_graph[pod_name]["innerIp"] = endpoint_item["status"]["networking"]["addressing"][0]["ipv4"]
+        data_for_graph[pod_name][utils.score_types.global_score] \
+            = microservices_yaml_handler.get_pod_score(pod_name, utils.score_types.global_score)
+        data_for_graph[pod_name][utils.score_types.access_score] \
+            = microservices_yaml_handler.get_pod_score(pod_name, utils.score_types.access_score)
+        data_for_graph[pod_name]["containers"] = pod_container_image_name_list
+        data_for_graph[pod_name]["containerInfos"] = container_info_list
+        data_for_graph[pod_name]["endpointInfos"] = endpoint_item
+
+    microservices_topology = MicroservicesTopology(microservice_name, data_for_graph, services_inner_ip_list)
+
+    microservices_topology.generate_graph_from_data()
+
+    # node是图论定义的节点，不是K8S的概念
+    # 这个方法必须执行，因为涉及到边的权重的计算
+    initial_node, paths = microservices_topology.get_initial_node_and_shortest_path()
+
+    if pic_output:
+        microservices_topology.generate_simple_picture()
+    if html_output:
+        microservices_topology.draw_graph_with_pyecharts()
+    # 保存原始输出信息以供调试，第三个参数为True表示删除原有的测试输出
+    if test_yaml_output:
+        output_for_test(microservices_yaml_handler, cilium_client, False)
--- microservices_topology/microservices_topology.py
+++ microservices_topology/microservices_topology.py
@@ -1,25 +1,107 @@
 # microservices_topology/microservices_topology.py
 import networkx as nx
 import matplotlib.pyplot as plt
+import pathlib
+
+from utils import utils
+from pyecharts import options as opts
+from pyecharts.charts import Graph
 
 
 class MicroservicesTopology:
-    def __init__(self):
-        self.graph = nx.Graph()
+    def __init__(self, microservice_name, data_for_graph: dict, services_inner_ip_list: list):
+        self.microservice_name = microservice_name
+        self.graph = nx.DiGraph()
+        self.data_for_graph = data_for_graph
+        self.services_inner_ip_list = services_inner_ip_list
+        self.initial_node_shortest_paths = None
+        current_directory = pathlib.Path(__file__).parent.resolve()
+        self.graph_output_directory = current_directory / "../output" / microservice_name
 
     def add_pod(self, pod_name):
         self.graph.add_node(pod_name, type='pod')
 
     def add_container(self, pod_name, container_image_name):
         self.graph.add_node(container_image_name, type='container', parent_pod=pod_name)
-        self.graph.add_edge(pod_name, container_image_name)
+        self.graph.add_edge(pod_name, container_image_name, weight=100)
 
     def add_pod_connection(self, pod1, pod2, weight):
-        self.graph.add_edge(pod1, pod2, type='pod_connection', weight=1)
+        self.graph.add_edge(pod1, pod2, type='podConnection', weight=weight)
 
-    def generate_picture(self):
+    def are_ips_in_same_service(self, ip1, ip2):
+        for ip_set in self.services_inner_ip_list:
+            if ip1 in ip_set and ip2 in ip_set:
+                return True
+        return False
+
+    def generate_graph_from_data(self):
+        for pod in self.data_for_graph:
+            self.add_pod(pod)
+            for container in self.data_for_graph[pod]["containers"]:
+                self.add_container(pod, container)
+
+        for pod1 in self.data_for_graph:
+            for pod2 in self.data_for_graph:
+                pod1_score = self.data_for_graph[pod1][utils.score_types.global_score]
+                pod2_score = self.data_for_graph[pod2][utils.score_types.global_score]
+                if self.are_ips_in_same_service(self.data_for_graph[pod1]["innerIp"],
+                                                self.data_for_graph[pod2]["innerIp"]):
+                    pod1_score += 100
+                    pod2_score += 100
+                if pod1 != pod2:
+                    self.add_pod_connection(pod1, pod2, 1 / pod1_score)
+                    self.add_pod_connection(pod2, pod1, 1 / pod2_score)
+
+    def get_shortest_path_by_node(self, node):
+        return nx.shortest_path(self.graph, source=node, weight='weight')
+
+    def get_initial_node_and_shortest_path(self):
+        for pod in self.data_for_graph:
+            if self.data_for_graph[pod][utils.score_types.access_score] > 0:
+                self.initial_node_shortest_paths = self.get_shortest_path_by_node(pod)
+                return pod, self.initial_node_shortest_paths
+
+    def generate_simple_picture(self):
         pos = nx.spring_layout(self.graph)
-        nx.draw(self.graph, pos, with_labels=True, node_color='skyblue', edge_color='black', node_size=2000, font_size=10,
+        nx.draw(self.graph, pos, with_labels=True, node_color='skyblue', edge_color='black', node_size=2000,
+                font_size=10,
                 font_weight='bold')
+        # 添加边的标签
+        edge_labels = nx.get_edge_attributes(self.graph, 'weight')
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels)
 
         plt.show()
+
+    def draw_graph_with_pyecharts(self):
+        nodes = [{"name": node, "symbolSize": 10} for node in self.graph.nodes()]
+        links = []
+        categories = [{"name": "Pod"}, {"name": "Container"}]
+
+        # 获取最短路径上的边
+        shortest_path_edges = set()
+        if self.initial_node_shortest_paths:
+            for path in self.initial_node_shortest_paths.values():
+                for i in range(len(path) - 1):
+                    shortest_path_edges.add((path[i], path[i + 1]))
+
+        # 添加边，设置不同的样式
+        for u, v in self.graph.edges():
+            if (u, v) in shortest_path_edges or (v, u) in shortest_path_edges:
+                links.append({"source": u, "target": v, "lineStyle": {"normal": {"color": "red", "width": 2}}})
+            else:
+                links.append({"source": u, "target": v})
+
+        # 更新节点类型
+        for node in nodes:
+            node_type = self.graph.nodes[node["name"]]["type"]
+            if node_type == "pod":
+                node["category"] = 0
+            else:  # container
+                node["category"] = 1
+
+        # 创建图表
+        g = Graph(init_opts=opts.InitOpts(width="100%", height="800px"))
+        g.add("", nodes, links, categories, repulsion=8000)
+
+        # 保存为 HTML 文件
+        g.render(self.graph_output_directory / (self.microservice_name + ".html"))
